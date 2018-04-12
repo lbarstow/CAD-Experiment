af57046c3fbd57ffd5105daea1752ce9efec5382
lbarstow <lbarstow@macalester.edu>
2018-04-12 01:23:25 -0400

    Length label now shows correct number

--- testgui.py
+++ testgui.py
@@ -14,8 +14,8 @@ class MyFirstGUI:
         self.name_entry_text = StringVar()
         self.name_label_text = StringVar()
         self.name_label_text.set("Name:")
-        self.name_length_text = IntVar()
-        self.name_length_text.set(self.name_length)
+        self.name_length_text = StringVar()
+        self.name_length_text.set("Length:")
 
 
         self.name_entry = Entry(master, textvariable=self.name_entry_text)
@@ -33,10 +33,12 @@ class MyFirstGUI:
         self.close_button.pack()
 
     def submit_name(self):
-        self.name_length = len(self.name_entry.get())
-        print(self.name_length)
         self.name = self.name_entry.get()
-        self.name_label_text.set("Name: " + self.name.get())
+        self.name_length = len(self.name)
+        print(self.name)
+        print(self.name_length)
+        self.name_label_text.set("Name: " + self.name)
+        self.name_length_text.set("Length: " + str(self.name_length))
 
 root = Tk()
 my_gui = MyFirstGUI(root)
