2d2fb4e98529567724f78c8899fb8831e63347b5
lbarstow <lbarstow@macalester.edu>
2018-04-12 01:17:43 -0400

    added length label. need to make it functional

--- testgui.py
+++ testgui.py
@@ -1,24 +1,42 @@
-from tkinter import Tk, Label, Button, Entry, StringVar
+from tkinter import Tk, Label, Button, Entry, StringVar, Text, IntVar
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
         master.title("A simple GUI")
+        master["bg"] = "orchid4"
 
-        self.label = Label(master, text="Enter the Name You Want Printed")
+        self.label = Label(master, text="Enter the Name You Want Printed:")
         self.label.pack()
 
         self.name = StringVar()
+        self.name_length = 0
 
-        self.name_box = Entry(master, textvariable=self.name)
-        self.name_box.pack()
+        self.name_entry_text = StringVar()
+        self.name_label_text = StringVar()
+        self.name_label_text.set("Name:")
+        self.name_length_text = IntVar()
+        self.name_length_text.set(self.name_length)
 
 
-        self.greet_button = Button(master, text="Print The Name", command=self.print_name)
-        self.greet_button.pack()
+        self.name_entry = Entry(master, textvariable=self.name_entry_text)
+        self.name_entry.pack()
 
+        self.submit_button = Button(master, text="Print The Name", command=self.submit_name)
+        self.submit_button.pack()
 
-    def print_name(self):
-        print(self.name_box.get())
+        self.name_label = Label(master, textvariable = self.name_label_text)
+        self.name_label.pack()
+        self.length_label = Label(master, textvariable = self.name_length_text)
+        self.length_label.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def submit_name(self):
+        self.name_length = len(self.name_entry.get())
+        print(self.name_length)
+        self.name.set(self.name_entry.get())
+        self.name_label_text.set("Name: " + self.name.get())
 
 root = Tk()
 my_gui = MyFirstGUI(root)
